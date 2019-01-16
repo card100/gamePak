12a3d628a3449428b050d0365111457675e92d42
Henry Gruett <gh22213@students.mcpasd.k12.wi.us>
2019-01-16 09:53:54 -0600

    Added setZoom and setCostume

--- sprite.js
+++ sprite.js
@@ -1,7 +1,32 @@
 // glbal vars
 var sprites = [];
+var costumes = [];
+var sounds = [];
+var zoom = [];
 
 // Creates new sprite
-function newSprite(name, costumes, sounds, zoom) {
-  
+function newSprite(name, costume, sounds, zoom) {
+  sprites.push(name);
+  costums.push(costumes);
+  sounds.push(sounds);
+  zoom.push(zoom);
+}
+
+function setCostume(name, costume, zoom) { // zoom is optional
+  try {
+    costumes[sprites.find(name)] = costume;
+    if (zoom !== null) {
+      zoom[sprites.find(name)] = zoom;
+    }
+  } catch (error) {
+    console.log("Error: Sprite "+name+" was requested but not found.");//dev mode
+  }
+}
+
+function setZoom(name, zoom) {
+  try {
+    zoom[sprites.find(name)] = zoom;
+  } catch (error) {
+    console.log("Error: Sprite "+name+" was requested but not found.");//dev mode
+  }
 }
